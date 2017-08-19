4bababf3dd143d4da2d89eb7748748ec74933d4b
--help <=>
2017-08-19 23:14:34 +0200

    initial commit

---
+++ jsonworld/src/main/java/com/pezhmankasraee/jsonworld/App.java
@@ -0,0 +1,13 @@
+package com.pezhmankasraee.jsonworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
